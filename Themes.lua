ba5355149fb397cb6b9fb025b71f39bd33a1503b
LoCal <114352935+Team1234Z@users.noreply.github.com>
2025-04-02 11:32:28 +0700

    Add files via upload

---
+++ Themes.lua
@@ -0,0 +1,10 @@
+return {
+    Dark = {
+        Name = "Dark",
+        Accent = "#111111",
+        Outline = "#FFFFFF",
+        Text = "#FFFFFF",
+        PlaceholderText = "#999999",
+    },
+    
+}
